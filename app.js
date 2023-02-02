b91e34dd719938a6517e6e480c81f3914d61d055
MuhammadNuman24 <numanrazzaq24@gmail.com>
2023-02-02 14:14:35 +0000

    initial commity

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+// Set Template Engine
+app.set("view engine", "ejs");
+// app.engine("html", require("ejs").renderFile);
+
+
+// app.use("/", require("./routes/home.routes"));
+app.use("/", require("./routes/home.routes"));
+
+app.use("/v1/api", require("./routes/orderDetails.routes"));
+
+const server = () => {
+  try {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+  } catch (error) {
+    console.log("Server ERROR:->" + error);
+  }
+};
+server();
